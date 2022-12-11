43cbd393182bc5f9bc988d6b8695cfa0f5cc52e2
mickeykerr <58183089+mickeykerr@users.noreply.github.com>
2022-12-11 13:19:37 -0800

    Update index.php
    Changed item from my ip to some-ip.

--- index.php
+++ index.php
@@ -80,7 +80,7 @@
                 <span class = "video-label">Video Feed</span>
             </div>
             <div>
-                <iframe src="http://71.93.65.129:8000/stream.mjpg" height="480" width="640">
+                <iframe src="http://some-ip:8000/stream.mjpg" height="480" width="640">
                 </iframe> 
             </div>
         </section>
